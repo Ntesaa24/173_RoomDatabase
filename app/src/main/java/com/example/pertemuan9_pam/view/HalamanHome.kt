6d488a31491daf8f8927da199b71ef45acad8a49
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-11-28 16:30:25 +0700

    menambahkan HalamanHome.kt

--- app/src/main/java/com/example/pertemuan9_pam/view/HalamanHome.kt
+++ app/src/main/java/com/example/pertemuan9_pam/view/HalamanHome.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -152,6 +153,11 @@ fun Datasiswa(
 		}
 	}
 }
+}
+
+@Composable
+fun Datasiswa(siswa: Siswa, modifier: Modifier) {
+	TODO("Not yet implemented")
 }
 
 
