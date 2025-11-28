35c182c1be0808876188677440d6d86acdfc15ed
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-11-28 16:29:49 +0700

    menambahkan HomeViewModel.kt

---
+++ app/src/main/java/com/example/pertemuan9_pam/viewmodel/HomeViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.pertemuan9_pam.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan9_pam.repositori.RepositoriSiswa
+import com.example.pertemuan9_pam.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+	companion object {
+		private const val TIMEOUT_MILLIS = 5_000L
+	}
+
+	val homeUiState: StateFlow<HomeUIState> = repositoriSiswa.getAllSiswaStream()
+		.filterNotNull()
+		.map { HomeUIState(listSiswa = it.toList())}
+		.stateIn(scope = viewModelScope,
+			started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+			initialValue = HomeUIState())
+
+	data class HomeUIState(
+		val listSiswa: List<Siswa> = listOf()
+	)
+}
+
