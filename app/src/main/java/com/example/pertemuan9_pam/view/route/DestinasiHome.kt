4dff68c7e48c20b6e70fd5745ad7709a143681ce
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-11-28 11:40:04 +0700

    menambahkan DestinasiHome.kt.kt

---
+++ app/src/main/java/com/example/pertemuan9_pam/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan9_pam.view.route
+
+import com.example.pertemuan9_pam.R
+
+object DestinasiHome: DestinasiNavigasi {
+	override val route = "home"
+	override val titleRes = R.string.app_name
+}
