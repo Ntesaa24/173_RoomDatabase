279cb12672d121155ac3d56e965fc547c4bd5b06
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-12-05 12:54:36 +0700

    perbaikan peta navigasi

--- app/src/main/java/com/example/pertemuan9_pam/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/pertemuan9_pam/view/uicontroller/PetaNavigasi.kt
@@ -4,11 +4,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pertemuan9_pam.DetailSiswaScreen
 import com.example.pertemuan9_pam.view.EntrySiswaScreen
 import com.example.pertemuan9_pam.view.HomeScreen
+import com.example.pertemuan9_pam.view.route.DestinasiDetaillSiswa
+import com.example.pertemuan9_pam.view.route.DestinasiDetaillSiswa.itemIdArg
 import com.example.pertemuan9_pam.view.route.DestinasiEntry
 import com.example.pertemuan9_pam.view.route.DestinasiHome
 
@@ -34,6 +39,9 @@ fun HostNavigasi(
 			HomeScreen(
 				navigateToItemEntry = {
 					navController.navigate(DestinasiEntry.route)
+				},
+				navigateToItemUpdate = {
+					navController.navigate("${DestinasiDetaillSiswa.route}/${it}")
 				}
 			)
 		}
@@ -43,6 +51,22 @@ fun HostNavigasi(
 				navigateBack = { navController.popBackStack() }
 			)
 		}
+		composable(
+			route = DestinasiDetaillSiswa.routeWithArgs,
+			arguments = listOf(navArgument(itemIdArg) {
+				type = NavType.IntType
+			})
+		) {
+			DetailSiswaScreen(
+				navigateToEditItem = { navController.navigate("${DestinasiEditSiswa.route}/$it")},
+				navigateBack = {navController.navigateUp() })
+		}
+		composable (route = DestinasiEditSiswa.routeWithArgs,
+			arguments = listOf(navArgument(DestinasiEditSiswa.itemIdArg){
+				type= NavType.IntType })){
+			EditSiswaScreen(
+				navigateBack = {navController.popBackStack()},
+				onNavigateUp = {navController.navigateUp()})
+		}
 	}
 }
-
