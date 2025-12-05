f016727e81210212b4f874379267d4377dc1990d
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-12-05 12:53:29 +0700

    perbaikan di repositori

--- app/src/main/java/com/example/pertemuan9_pam/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9_pam/repositori/RepositoriSiswa.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.flow.Flow
 interface  RepositoriSiswa {
 	fun getAllSiswaStream(): Flow<List<Siswa>>
 	suspend fun insertSiswa(siswa: Siswa)
+	//edit 1 tambah 3 fungsi berikut
+	fun getSiswaStream(id: Int): Flow<Siswa?>
+	suspend fun deleteSiswa(siswa: Siswa)
 }
 
 class OffLinedRepositoriSiswa(
@@ -14,4 +17,9 @@ class OffLinedRepositoriSiswa(
 ): RepositoriSiswa {
 	override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
 	override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+	//edit 2 tambah 3 overide berikut
+	override fun getSiswaStream(id: Int): Flow<Siswa?> =siswaDao.getSiswa(id)
+	override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+	//override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 	}
